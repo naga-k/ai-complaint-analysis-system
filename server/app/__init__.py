eade916f77928071155a09fbe8db6e846947bc31
Siddharth Kundu <sk31395@gmail.com>
2024-08-18 04:19:57 -0400

    get main files

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 # from config import Config
 from .extensions import init_extensions
 from .controllers.complaint_controller import complaint_blueprint
-from .controllers.video_controller import video_bp
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +9,5 @@ def create_app():
     init_extensions()
     
     app.register_blueprint(complaint_blueprint, url_prefix='/complaint')
-    app.register_blueprint(video_bp, url_prefix='/video')
     
     return app
